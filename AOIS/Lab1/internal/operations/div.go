d79f602e2ec31ca7d16b8cf476cf0ab6cb6563d9
kitanoyoru <kitanoyoru@protonmail.com>
2023-02-25 09:09:42 +0300

    Dev

--- AOIS/Lab1/internal/operations/div.go
+++ AOIS/Lab1/internal/operations/div.go
@@ -1,5 +1,35 @@
 package operations
 
+import (
+	"github.com/kitanoyoru/Labs/AOIS/Lab1/internal/code"
+	"github.com/kitanoyoru/Labs/AOIS/Lab1/internal/constants"
+	"github.com/kitanoyoru/Labs/AOIS/Lab1/internal/raw"
+)
+
 func Div(a, b int) []byte {
-	return []byte{}
+	x := code.GetStraightCode(a)
+	y := code.GetStraightCode(b)
+
+	var sign bool
+	if (a < 0 && b > 0) || (a > 0 && b < 0) {
+		sign = true
+	}
+
+	x[0], y[0] = 0, 0
+
+	res := constants.Zero32InBytes
+	for GreaterOrEqual(x, y) {
+		y[0] = 1
+		x = raw.RawSum(x, code.Straight2Additional(y))
+		res = raw.RawSum(res, constants.One32InBytes)
+		y[0] = 0
+	}
+
+	if sign {
+		res = append([]byte{1}, res...)
+	} else {
+		res = append([]byte{0}, res...)
+	}
+
+	return res
 }
