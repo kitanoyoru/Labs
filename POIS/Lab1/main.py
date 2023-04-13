285bd9539a508403fe27c39e25f8d44c6eac63c5
kitanoyoru <kitanoyoru@protonmail.com>
2023-04-13 04:19:04 +0300

    solved some problems

--- POIS/Lab1/main.py
+++ POIS/Lab1/main.py
@@ -1,7 +1,6 @@
 import os
 
 from argparse import ArgumentParser
-from gevent.pywsgi import WSGIServer
 
 from app import App
 
@@ -11,23 +10,15 @@ HOST = "localhost"
 PORT = 8080
 
 
-def main() -> None:
-    parser = ArgumentParser(prog=PROJECT_NAME, usage="%(prog)s [options]")
-    parser.add_argument(
-        "--plants",
-        help="How many plants simulation need to have",
-        default=3,
-    )
+parser = ArgumentParser(prog=PROJECT_NAME, usage="%(prog)s [options]")
+parser.add_argument(
+    "--plants",
+    help="How many plants simulation need to have",
+    default=3,
+)
 
-    args = parser.parse_args()
+args = parser.parse_args()
 
-    settings_path = os.path.join("config", ".env")
+settings_path = os.path.join("config", ".env")
 
-    app = App.create_app(args.plants, settings_path)
-
-    http_server = WSGIServer((HOST, PORT), app)
-    http_server.serve_forever()
-
-
-if __name__ == "__main__":
-    main()
+app = App.create_app(args.plants, settings_path)
